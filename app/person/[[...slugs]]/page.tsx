242b106e8a3f649e619ef23e30d60a864830d2ff
Kuma Bayne <kuma88@me.com>
2025-05-26 21:40:59 -0700

    Update person CSS

--- app/person/[[...slugs]]/page.tsx
+++ app/person/[[...slugs]]/page.tsx
@@ -5,11 +5,14 @@ import { PersonDetailsType } from "@/types/people";
 import { calculateAge, formatDate } from "@/utils/helpers";
 import Rating from "@/components/rating";
 import Credits from "@/src/components/Credits";
-import KnownFor from "@/components/known-for";
 import Bio from "@/components/bio";
+import Typography from "@/components/typography";
+import MediaRow from "@/components/media-row";
+import { MovieType } from "@/types/movie";
+import PersonSocial from "@/components/person-social";
 
 async function getData(id: string) {
-  const url = `https://api.themoviedb.org/3/person/${id}?language=en-US&append_to_response=combined_credits`;
+  const url = `https://api.themoviedb.org/3/person/${id}?language=en-US&append_to_response=combined_credits,images,external_ids`;
 
   const res = await fetch(url, {
     method: "GET",
@@ -44,77 +47,77 @@ export default async function PersonPage(props: {
   const knownFor = data.combined_credits.cast
     .slice(0)
     .sort((a, b) => b.vote_count - a.vote_count)
-    .slice(0, 4);
+    .slice(0, 5) as MovieType[];
 
   return (
-    <div className="pt-16 lg:pt-6">
+    <main className="pt-16">
       <Container>
-        <div className="mb-4 grid grid-cols-profile gap-2 md:grid-cols-[120px_1fr]">
-          <div className="relative">
-            <Image
-              className="rounded"
-              src={`${configuration.images.secure_base_url}${configuration.images.profile_sizes[3]}${data.profile_path}`}
-              alt={data.name}
-              width="1920"
-              height="1080"
-            />
-            <div className="absolute bottom-1 right-1">
-              <Rating rating={Math.round(data.popularity)} fixed={0} />
+        <div className="xl:grid xl:grid-cols-2">
+          <div className="mb-4 grid grid-cols-[120px_1fr] items-start gap-2 md:grid-cols-[160px_1fr] md:gap-6">
+            <div className="xl xl:grid">
+              <div className="relative">
+                <Image
+                  className="rounded-xl"
+                  src={`${configuration.images.secure_base_url}${configuration.images.profile_sizes[3]}${data.profile_path}`}
+                  alt={data.name}
+                  width="1920"
+                  height="1080"
+                />
+                <div className="absolute bottom-1 right-1">
+                  <Rating rating={Math.round(data.popularity)} fixed={0} />
+                </div>
+              </div>
+            </div>
+            <div className="space-y-2 md:self-end">
+              <div className="mb-2 md:mb-0">
+                <Typography
+                  as="h1"
+                  className="text-xl font-semibold text-zinc-100"
+                >
+                  {data.name}
+                </Typography>
+                <div className="flex flex-col flex-wrap gap-0.5 md:flex-row md:gap-6">
+                  <div>
+                    <p className="text-sm font-bold">Born</p>
+                    <p className="text-sm">
+                      {formatDate(data.birthday)}
+                      {!data.deathday &&
+                        ` (${calculateAge(data.birthday)} years old)`}
+                    </p>
+                  </div>
+                  {data.deathday && (
+                    <div>
+                      <p className="text-sm font-bold">Died</p>
+                      <p className="text-sm">
+                        {`${formatDate(data.deathday)} (${calculateAge(
+                          data.birthday,
+                          data.deathday,
+                        )} years old)`}
+                      </p>
+                    </div>
+                  )}
+                  <div>
+                    <p className="text-sm font-bold">Known For</p>
+                    <p className="text-sm">{data.known_for_department}</p>
+                  </div>
+                  <div>
+                    <p className="text-sm font-bold">Place of Birth</p>
+                    <p className="text-sm">{data.place_of_birth}</p>
+                  </div>
+                </div>
+              </div>
+              <PersonSocial data={data.external_ids} />
             </div>
           </div>
-          <div className="md:self-end">
-            <div className="mb-2 md:mb-0">
-              <h1 className="text-xl font-semibold text-zinc-100">
-                {data.name}
-              </h1>
-              <p className="text-xs text-zinc-400 md:text-base">
-                <span className="font-extrabold md:font-semibold">Born</span>
-                <span className="font-semibold md:font-normal">
-                  &nbsp;&middot;&nbsp;
-                </span>
-                <span className="font-semibold md:font-normal">
-                  {formatDate(data.birthday)}
-                  {!data.deathday &&
-                    ` (${calculateAge(data.birthday)} years old)`}
-                </span>
-              </p>
-              {data.deathday && (
-                <p className="text-xs text-zinc-400 md:text-base">
-                  <span className="font-extrabold md:font-semibold">Died</span>
-                  <span className="font-semibold md:font-normal">
-                    &nbsp;&middot;&nbsp;
-                  </span>
-                  <span className="font-semibold md:font-normal">
-                    {`${formatDate(data.deathday)} (${calculateAge(
-                      data.birthday,
-                      data.deathday,
-                    )} years old)`}
-                  </span>
-                </p>
-              )}
-            </div>
-            <div className="hidden md:block">
-              <p className="text-xs text-zinc-400 md:text-base">
-                <b>Known For:</b> {data.known_for_department}
-              </p>
-              <p className="text-xs text-zinc-400 md:text-base">
-                <b>Place of Birth:</b> {data.place_of_birth}
-              </p>
-            </div>
-            <div className="md:hidden">
-              <Bio data={data} />
-            </div>
+          <div className="col-span-full">
+            <Bio data={data} />
           </div>
         </div>
-        <div className="mb-4 hidden md:block">
-          <h2 className="col-span-full font-semibold text-zinc-100">Bio</h2>
-          <p className="text-zinc-400">{data.biography}</p>
-        </div>
-        <div className="max-w-md">
-          <KnownFor knownFor={knownFor} />
+        <div className="xl:mt-6">
+          <MediaRow data={{ results: knownFor }} />
         </div>
         <Credits credits={castCredits} />
       </Container>
-    </div>
+    </main>
   );
 }
--- components/bio.tsx
+++ components/bio.tsx
@@ -1,83 +1,35 @@
 "use client";
 
+import { useState } from "react";
 import { PersonDetailsType } from "../types/people";
-import Image from "next/image";
-import { configuration } from "@/utils/data";
-import { calculateAge, formatDate } from "@/utils/helpers";
-import Rating from "./rating";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { IconChevronRight } from "@tabler/icons-react";
+import { cn } from "@/lib/utils";
 
 export default function Bio({ data }: { data: PersonDetailsType }) {
+  const [open, setOpen] = useState(false);
   const paragraphs = data.biography.split("\n\n");
 
   return (
-    <Dialog>
-      <DialogTrigger className="flex text-sm text-pink-500">
-        <p className="line-clamp-4 text-left text-xs text-zinc-400">
-          {data.biography}
-        </p>
-        <div className="self-end text-zinc-200">
-          <IconChevronRight className="h-4 w-4" />
-        </div>
-      </DialogTrigger>
-      <DialogContent className="max-w-[90%]">
-        <div className="mt-4 max-h-96 overflow-scroll pr-4">
-          <div className="mb-4 grid grid-cols-profile gap-2">
-            <div className="relative self-start">
-              <Image
-                className="rounded"
-                src={`${configuration.images.secure_base_url}${configuration.images.profile_sizes[3]}${data.profile_path}`}
-                alt={data.name}
-                width="1920"
-                height="1080"
-              />
-              <div className="absolute bottom-1 right-1">
-                <Rating rating={Math.round(data.popularity)} fixed={0} />
-              </div>
-            </div>
-            <div>
-              <div className="mb-2">
-                <h1 className="text-xl font-semibold text-zinc-100">
-                  {data.name}
-                </h1>
-                <p className="text-xs text-zinc-400">
-                  <span className="font-extrabold">Born</span>
-                  <span className="font-semibold">&nbsp;&middot;&nbsp;</span>
-                  <span className="font-semibold">
-                    {formatDate(data.birthday)}
-                    {!data.deathday &&
-                      ` (${calculateAge(data.birthday)} years old)`}
-                  </span>
-                </p>
-                {data.deathday && (
-                  <p className="text-xs text-zinc-400">
-                    <span className="font-extrabold">Died</span>
-                    <span className="font-semibold">&nbsp;&middot;&nbsp;</span>
-                    <span className="font-semibold">
-                      {`${formatDate(data.deathday)} (${calculateAge(
-                        data.birthday,
-                        data.deathday,
-                      )} years old)`}
-                    </span>
-                  </p>
-                )}
-              </div>
-              <p className="text-xs text-zinc-400">
-                <b>Known For:</b> {data.known_for_department}
-              </p>
-              <p className="text-xs text-zinc-400">
-                <b>Place of Birth:</b> {data.place_of_birth}
-              </p>
-            </div>
-          </div>
-          {paragraphs.map((item, i) => (
-            <p key={i} className="mb-4 text-sm text-zinc-400">
-              {item}
-            </p>
-          ))}
-        </div>
-      </DialogContent>
-    </Dialog>
+    <div>
+      <div
+        className={cn(
+          "space-y-2",
+          !open && "line-clamp-4",
+          open && "line-clamp-none",
+        )}
+      >
+        {paragraphs.map((item, i) => (
+          <p key={i}>{item}</p>
+        ))}
+      </div>
+      <div className="flex justify-end">
+        <button
+          className="text-sm font-medium"
+          onClick={() => setOpen(!open)}
+          type="button"
+        >
+          {open ? "Read Less" : "Read More"}
+        </button>
+      </div>
+    </div>
   );
 }
